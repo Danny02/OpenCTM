b05c55f783a9b516762174d21846309eeda2a13d
marcus256 <marcus256@0f83b9c6-fc7f-44b6-b5ec-6ccadc43c738>
2009-08-29 06:35:45 +0000

    Added a script for building the source code distribution archives.

---
+++ build-src.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Name of the distribution
+distname=OpenCTM-0.7
+
+# Clean the source directories
+echo Cleaning up...
+make -f Makefile.linux clean
+cd tools
+make -f Makefile.linux clean
+cd jpeg
+make -f Makefile.linux clean
+cd ../..
+
+# Build all the necessary files
+echo Building documentation...
+cd doc
+doxygen
+pdflatex DevelopersManual.tex
+pdflatex DevelopersManual.tex
+cd ..
+
+# Set up a temporary directory
+tmproot=/tmp/openctm-$USER-$$
+mkdir $tmproot
+tmpdir=$tmproot/$distname
+mkdir $tmpdir
+
+# Copy files
+echo Copying files to $tmpdir...
+cp *.c *.h *.rc *.def Makefile* *.txt $tmpdir/
+mkdir $tmpdir/liblzma
+cp liblzma/* $tmpdir/liblzma/
+mkdir $tmpdir/tools
+cp tools/*.cpp tools/*.h tools/*.ico tools/*.vert tools/*.frag tools/*.rc tools/Makefile* $tmpdir/tools/
+mkdir $tmpdir/tools/jpeg
+cp tools/jpeg/* $tmpdir/tools/jpeg/
+mkdir $tmpdir/tools/rply
+cp tools/rply/* $tmpdir/tools/rply/
+mkdir $tmpdir/tools/glew
+cp -R tools/glew/* $tmpdir/tools/glew/
+
+mkdir $tmpdir/doc
+cp doc/DevelopersManual.pdf $tmpdir/doc/
+cp doc/format.txt $tmpdir/doc/
+mkdir $tmpdir/doc/APIReference
+cp doc/APIReference/* $tmpdir/doc/APIReference/
+
+mkdir $tmpdir/plugins
+mkdir $tmpdir/plugins/blender
+cp plugins/blender/* $tmpdir/plugins/blender/
+
+mkdir $tmpdir/bindings
+mkdir $tmpdir/bindings/delphi
+cp bindings/delphi/* $tmpdir/bindings/delphi/
+
+# Create archives
+tar -cvf $tmproot/$distname.tar $tmpdir
