efaf5f9ff74e2ebebd388633a9ecc5f07589125e
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2012-10-22 00:02:04 +0200

    cleanup

--- js/mesh2ctm.js
+++ js/mesh2ctm.js
@@ -2,30 +2,30 @@ var exec = require('child_process').exec,
 	libPath=require('path'),
 	settings=require('./config.json');
 
-var vtkSurfacePath=settings.vtkSurfacePath;
-var ctmConvPath=settings.ctmConvPath;
+var vtkSurfacePath = settings.vtkSurfacePath;
+var ctmConvPath = __dirname + '/../tools/';
 
-var ctmConvCommand=ctmConvPath+"ctmconv";
-var ctmVConvEnv={LD_LIBRARY_PATH : ctmConvPath};
+var ctmConvCommand = ctmConvPath + "ctmconv";
+var ctmVConvEnv = {LD_LIBRARY_PATH : ctmConvPath};
 
-exports.execute=function (parameters, callback) {
-	var filesRoot=parameters.filesRoot;
-	var inputMesh=filesRoot+parameters.input_mesh;
-	var outputDirectory=filesRoot+parameters.output_directory;
+exports.execute = function (parameters, callback) {
+	var filesRoot = parameters.filesRoot;
+	var inputMesh = filesRoot+parameters.input_mesh;
+	var outputDirectory = filesRoot+parameters.output_directory;
 
-	var execOptions={cwd :outputDirectory, env: ctmVConvEnv};
-	var plyMesh="mesh.ply";
+	var execOptions = {cwd :outputDirectory, env: ctmVConvEnv};
+	var plyMesh = "mesh.ply";
 
 	switch (libPath.extname(inputMesh))
 	{
 	case ".vtk":
-		exec(vtkSurfacePath+"vtk2ply "+inputMesh, execOptions, ctmConv);
+		exec(vtkSurfacePath + "vtk2ply " + inputMesh, execOptions, ctmConv);
 		break;
 	case ".stl":
-		exec(vtkSurfacePath+"stl2ply "+inputMesh, execOptions, ctmConv);
+		exec(vtkSurfacePath + "stl2ply " + inputMesh, execOptions, ctmConv);
 		break;
 	default:
-		plyMesh=inputMesh;
+		plyMesh = inputMesh;
 		ctmConv(null);
 	}
 
@@ -33,7 +33,7 @@ exports.execute=function (parameters, callback) {
 		if (err) {
 			callback(err, stdout, stderr);
 		}
-		exec(ctmConvCommand+" "+plyMesh+" mesh.ctm", execOptions, function (err, stdout, stderr) {
+		exec(ctmConvCommand + " " + plyMesh + " mesh.ctm", execOptions, function (err, stdout, stderr) {
 				callback(err, stdout, stderr);
 		});
 	}
